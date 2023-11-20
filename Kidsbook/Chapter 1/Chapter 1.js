446da171ebc94d333b2662dd76bba8c3745dff47
Bokang412 <151556695+Bokang412@users.noreply.github.com>
2023-11-20 19:03:16 +0200

    Add files via upload

---
+++ Kidsbook/Chapter 1/Chapter 1.js
@@ -0,0 +1,14 @@
+// Writing some Javascript
+
+console.log(3 + 4);
+
+// The structure of Javascript program
+
+// Draw as many cats as you want!
+let drawCats = function (howManyTimes) {
+    for (let i = 0; i < howManyTimes; i++) {
+    console.log(i + " =^.^=");
+    }
+   };
+   drawCats(10); // You can put any number here instead of 10.
+   
